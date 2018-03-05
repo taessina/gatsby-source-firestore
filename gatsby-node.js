aac6195dac545d7339b0d2ba5e07892facf82932
Bailey Griswold <baymose@gmail.com>
2018-03-05 14:13:57 -0600

    Update to use Object.assign, instead of map
    I was running into a weird compatibility issue with gatsby/babel. Could you maybe you object.assign instead?
    Thanks!

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,41 +1,42 @@
-const firebase = require("firebase-admin");
+const firebase = require('firebase-admin');
 const crypto = require('crypto');
 
+const getDigest = id =>
+  crypto
+    .createHash('md5')
+    .update(id)
+    .digest('hex');
 
-const getDigest = id => crypto
-  .createHash('md5')
-  .update(id)
-  .digest('hex');
-
-exports.sourceNodes = async ({ boundActionCreators }, { types, credential }) => {
+exports.sourceNodes = async (
+  { boundActionCreators },
+  { types, credential }
+) => {
   firebase.initializeApp({ credential: firebase.credential.cert(credential) });
   const db = firebase.firestore();
 
   const { createNode, createNodeField } = boundActionCreators;
 
-  const promises = types.map(async ({
-    collection,
-    type,
-    populate,
-    map = node => node,
-  }) => {
-    const snapshot = await db.collection(collection).get();
-    for (let doc of snapshot.docs) {
-      const contentDigest = getDigest(doc.id);
-      const node = createNode({
-        ...map(doc.data()),
-        id: doc.id,
-        parent: null,
-        children: [],
-        internal: {
-          type,
-          contentDigest,
-        },
-      });
+  const promises = types.map(
+    async ({ collection, type, populate, map = node => node }) => {
+      const snapshot = await db.collection(collection).get();
+      for (let doc of snapshot.docs) {
+        const contentDigest = getDigest(doc.id);
+        const node = createNode(
+          Object.assign({}, map(doc.data()), {
+            id: doc.id,
+            parent: null,
+            children: [],
+            internal: {
+              type,
+              contentDigest,
+            },
+          })
+        );
 
-      Promise.resolve();
+        Promise.resolve();
+      }
     }
-  })
+  );
 
   await Promise.all(promises);
 
